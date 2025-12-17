7d4d2134ebb08c0710da487557f1c56838b110c3
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2025-12-17 18:27:11 +0600

    Add background color theme

--- src/components/ThemeToggleBtn.jsx
+++ src/components/ThemeToggleBtn.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import assets from '../assets/assets'
 
 const ThemeToggleBtn = ({theme, setTheme}) => {
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
   return (
     <>
       <button>
