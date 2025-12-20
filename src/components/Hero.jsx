1b0126b74ffd64dd3425fdea23c8967374afbc47
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2025-12-20 23:12:35 +0600

    Add hero section with headline and subheading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,33 @@ import assets from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div id='hero' className='flex flex-col gap-6 py-20 items-center px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white'>
+    <div
+      id="hero"
+      className="flex flex-col gap-6 py-20 items-center px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white"
+    >
+      <div className="inline-flex items-center border border-gray-300 p-1.5 pr-4 rounded-full gap-2">
+        <img className="w-20" src={assets.group_profile} alt="" />
+        <p className="text-xs font-medium">Trusted by 10k+ people</p>
+      </div>
 
-      <div className='inline-flex items-center border border-gray-300 p-1.5 pr-4 rounded-full gap-2'>
-        <img className='w-20' src={assets.group_profile} alt="" />
-        <p className='text-xs font-medium'>Trusted by 10k+ people</p>
+      <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl">
+        Turning imagination into{" "}
+        <span className="bg-gradient-to-r from-[#5044e5] to-[#4d8cea] bg-clip-text text-transparent">
+          digital
+        </span>{" "}
+        impact.
+      </h1>
+
+      <p className='sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-4/5 sm:max-w-lg pb-3 text-sm'>
+        Creating meaningful connections and turning big ideas into interactive
+        digital experiences.
+      </p>
+
+      <div className='relative'>
+        <img src={assets.hero_img} alt="" className='w-full max-w-6xl'/>
       </div>
     </div>
-  )
+  );
 }
 
 export default Hero
