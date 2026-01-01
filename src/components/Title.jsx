571476711ff4bd3cf6794c37e090f50f0bfbf964
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-01 11:23:43 +0600

    Add new component Title

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = ({title, desc}) => {
+  return (
+    <>
+      <h2 className='text-3xl sm:text-5xl font-medium'>{title}</h2>
+      <p className='text-center text-gray-500 dark:text-white/75 mb-6 max-w-xl'>{desc}</p>
+    </>
+  )
+}
+
+export default Title
