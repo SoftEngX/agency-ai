2a0d7edb513b791719237e7f016fb3fc20d72370
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-02 13:55:46 +0600

    Fix errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,22 +12,26 @@ const Hero = () => {
         <p className="text-xs font-medium">Trusted by 10k+ people</p>
       </div>
 
-      <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-23.75 max-w-5xl">
         Turning imagination into{" "}
-        <span className="bg-gradient-to-r from-[#5044e5] to-[#4d8cea] bg-clip-text text-transparent">
+        <span className="bg-linear-to-r from-primary to-[#4d8cea] bg-clip-text text-transparent">
           digital
         </span>{" "}
         impact.
       </h1>
 
-      <p className='sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-4/5 sm:max-w-lg pb-3 text-sm'>
+      <p className="sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-[80%] sm:max-w-lg pb-3 text-sm">
         Creating meaningful connections and turning big ideas into interactive
         digital experiences.
       </p>
 
-      <div className='relative'>
-        <img src={assets.hero_img} alt="" className='w-full max-w-6xl'/>
-        <img src={assets.bgImage1} alt="" className='absolute -top-40 -right-40 sm:-top-100 sm:-right-70 -z-1 dark:hidden'/>
+      <div className="relative">
+        <img src={assets.hero_img} alt="" className="w-full max-w-6xl" />
+        <img
+          src={assets.bgImage1}
+          alt=""
+          className="absolute -top-40 -right-40 sm:-top-100 sm:-right-70 -z-1 dark:hidden"
+        />
       </div>
     </div>
   );
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
 
@@ -45,6 +46,12 @@ const Services = () => {
         title="How can we help you?"
         desc="From strategy to execution, we craft digital solutions that move your business forward."
       />
+
+      <div className='flex flex-col md:grid grid-cols-2'>
+        {servicesData.map((service, index)=>(
+          <ServiceCard key={index} service={service} index={index} />
+        ))}
+      </div>
     </div>
   );
 }
