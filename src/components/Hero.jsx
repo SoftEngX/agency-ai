684b2cb99082e6d5f18c5b4ffd4619f9485ebf23
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-05 21:48:05 +0600

    Add animation for Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
+import { motion } from "motion/react";
+
 
 const Hero = () => {
   return (
@@ -7,32 +9,55 @@ const Hero = () => {
       id="hero"
       className="flex flex-col gap-6 py-20 items-center px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white"
     >
-      <div className="inline-flex items-center border border-gray-300 p-1.5 pr-4 rounded-full gap-2">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        viewport={{ once: true }}
+        className="inline-flex items-center border border-gray-300 p-1.5 pr-4 rounded-full gap-2"
+      >
         <img className="w-20" src={assets.group_profile} alt="" />
         <p className="text-xs font-medium">Trusted by 10k+ people</p>
-      </div>
+      </motion.div>
 
-      <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-23.75 max-w-5xl">
+      <motion.h1
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transitsion={{ duration: 0.6, delay: 0.8 }}
+        viewport={{ once: true }}
+        className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-23.75 max-w-5xl"
+      >
         Turning imagination into{" "}
         <span className="bg-linear-to-r from-primary to-[#4d8cea] bg-clip-text text-transparent">
           digital
         </span>{" "}
         impact.
-      </h1>
+      </motion.h1>
 
-      <p className="sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-[80%] sm:max-w-lg pb-3 text-sm">
+      <motion.p
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transitsion={{ duration: 0.5, delay: 1 }}
+        viewport={{ once: true }}
+        className="sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-[80%] sm:max-w-lg pb-3 text-sm">
         Creating meaningful connections and turning big ideas into interactive
         digital experiences.
-      </p>
+      </motion.p>
 
-      <div className="relative">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transitsion={{ duration: 0.6, delay: 2 }}
+        viewport={{ once: true }}
+        className="relative"
+      >
         <img src={assets.hero_img} alt="" className="w-full max-w-6xl" />
         <img
           src={assets.bgImage1}
           alt=""
           className="absolute -top-40 -right-40 sm:-top-100 sm:-right-70 -z-1 dark:hidden"
         />
-      </div>
+      </motion.div>
     </div>
   );
 }
