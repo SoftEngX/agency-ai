aaf0be76e36a4a16a5e08172a050f0dd020493b4
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-05 22:04:25 +0600

    Add animation for Titile component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { motion } from "motion/react";
+
 
 const Title = ({title, desc}) => {
 
   return (
     <>
-      <h2 className="text-3xl sm:text-5xl font-medium ">{title}</h2>
-      <p className="text-center text-gray-500 dark:text-white/75 mb-6 max-w-xl">
+      <motion.h2
+      inheritial={{ opacity: 0, y: 30 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+      viewport={{ once: true }}
+      className="text-3xl sm:text-5xl font-medium ">{title}</motion.h2>
+      <motion.p
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        viewport={{ once: true }}
+      className="text-center text-gray-500 dark:text-white/75 mb-6 max-w-xl">
         {desc}
-      </p>
+      </motion.p>
     </>
   );
 }
